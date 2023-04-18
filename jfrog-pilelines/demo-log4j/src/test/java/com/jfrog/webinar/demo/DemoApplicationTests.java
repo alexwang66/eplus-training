9a3a9f0e72bc64f331674ec9638b62c4e1482bdc
wq237wq@gmail.com <wq237wq@gmail.com>
2023-04-18 15:59:27 +0800

    add log4j demo

---
+++ jfrog-pilelines/demo-log4j/src/test/java/com/jfrog/webinar/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfrog.webinar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
